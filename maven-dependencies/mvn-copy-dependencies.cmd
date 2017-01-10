f95ca5f6a6876ba04134e60733e052bf2d10c7e7
Zor-X-L <liuxiaogenius@gmail.com>
2017-01-10 15:09:33 +0800

    stop upon failed commands

--- maven-dependencies/mvn-copy-dependencies.cmd
+++ maven-dependencies/mvn-copy-dependencies.cmd
@@ -2,16 +2,16 @@
 
 @ECHO OFF
 
-CALL mvn org.apache.maven.plugins:maven-dependency-plugin:2.10:sources
-REM CALL mvn org.apache.maven.plugins:maven-dependency-plugin:2.10:resolve -Dclassifier=javadoc
-CALL mvn org.apache.maven.plugins:maven-dependency-plugin:2.10:resolve
+CALL mvn org.apache.maven.plugins:maven-dependency-plugin:2.10:sources || EXIT /B
+REM CALL mvn org.apache.maven.plugins:maven-dependency-plugin:2.10:resolve -Dclassifier=javadoc || EXIT /B
+CALL mvn org.apache.maven.plugins:maven-dependency-plugin:2.10:resolve || EXIT /B
 
 RD /S /Q target
 DEL %1-maven-dependencies.zip
 
-CALL mvn org.apache.maven.plugins:maven-dependency-plugin:2.10:copy-dependencies -Dmdep.addParentPoms=true -Dmdep.copyPom=true -Dmdep.useRepositoryLayout=true -Dclassifier=sources
-REM CALL mvn org.apache.maven.plugins:maven-dependency-plugin:2.10:copy-dependencies -Dmdep.addParentPoms=true -Dmdep.copyPom=true -Dmdep.useRepositoryLayout=true -Dclassifier=javadoc
-CALL mvn org.apache.maven.plugins:maven-dependency-plugin:2.10:copy-dependencies -Dmdep.addParentPoms=true -Dmdep.copyPom=true -Dmdep.useRepositoryLayout=true
+CALL mvn org.apache.maven.plugins:maven-dependency-plugin:2.10:copy-dependencies -Dmdep.addParentPoms=true -Dmdep.copyPom=true -Dmdep.useRepositoryLayout=true -Dclassifier=sources || EXIT /B
+REM CALL mvn org.apache.maven.plugins:maven-dependency-plugin:2.10:copy-dependencies -Dmdep.addParentPoms=true -Dmdep.copyPom=true -Dmdep.useRepositoryLayout=true -Dclassifier=javadoc || EXIT /B
+CALL mvn org.apache.maven.plugins:maven-dependency-plugin:2.10:copy-dependencies -Dmdep.addParentPoms=true -Dmdep.copyPom=true -Dmdep.useRepositoryLayout=true || EXIT /B
 
 COPY pom.xml %1-maven-dependencies-pom.xml
 
