1d458b3de3c18c77e982a534bd7a26d6b55a86de
Zor-X-L <liuxiaogenius@gmail.com>
2017-01-17 11:30:06 +0800

    add utilities download and install scripts, including utilities for cluster management, monitoring, benchmarking, hardware info collecting

---
+++ utilities/download.sh
@@ -0,0 +1,7 @@
+../repoquery-recursive.py \
+	tmux python-ipython-console python34 pssh clusterssh \
+	inxi lshw \
+	sysbench bonnie++ fio iperf3 \
+	collectl glances atop statgrab-tools \
+	htop iotop \
+	iptraf-ng nethogs iftop vnstat ntop jnettop ngrep nload | wget -i -
---
+++ utilities/install.sh
@@ -0,0 +1,5 @@
+yum install \
+	screen hdparm dstat sysstat procps \
+	tcpdump net-tools arpwatch iproute ethtool traceroute ipstate nmap nc \
+	lsof psacct strace \
+	./*.rpm
