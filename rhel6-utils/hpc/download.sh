a6a4e1c6ed20accc996b83985f93345a6c97a12e
Zor-X-L <liuxiaogenius@gmail.com>
2017-01-24 10:19:04 +0800

    add hpc related stuff

---
+++ rhel6-utils/hpc/download.sh
@@ -0,0 +1,2 @@
+../repoquery-recursive.py openblas*.x86_64 | wget -i -
+#rm -f *i686*.rpm
---
+++ rhel6-utils/hpc/install.sh
@@ -0,0 +1,2 @@
+yum -y groupinstall 'Tex support'
+yum -y install gnuplot openmpi-devel mpitests-openmpi ./*.rpm
