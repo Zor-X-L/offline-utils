4b526a5afffc0e958dd80b576de3366c91f5a463
Zor-X-L <liuxiaogenius@gmail.com>
2017-03-03 11:39:10 +0800

    skip downloading if remote file is not newer than local file & fix bug

--- rhel6-utils/download.sh
+++ rhel6-utils/download.sh
@@ -5,13 +5,13 @@
 	collectl glances atop statgrab-tools \
 	htop iotop \
 	iptraf-ng nethogs iftop vnstat jnettop ngrep nload \
-	rh-git29-git-all
-	| wget -i - || exit 1
+	rh-git29-git-all \
+	| wget -Ni - || exit 1
 
 rm -f *i686*.rpm
 
-wget 'http://www.slac.stanford.edu/~abh/bbcp/bin/amd64_rhel60/bbcp' || exit 1
+wget -N 'http://www.slac.stanford.edu/~abh/bbcp/bin/amd64_rhel60/bbcp' || exit 1
 
-wget 'https://downloads.sourceforge.net/project/nmon/nmon16e_x86_rhel65'||exit 1
+wget -N 'https://downloads.sourceforge.net/project/nmon/nmon16e_x86_rhel65'||exit 1
 
-wget 'https://docs.broadcom.com/docs-and-downloads/raid-controllers/raid-controllers-common-files/8-07-14_MegaCLI.zip' || exit 1
+wget -N 'https://docs.broadcom.com/docs-and-downloads/raid-controllers/raid-controllers-common-files/8-07-14_MegaCLI.zip' || exit 1
