8cf1addd6e666336b9711da897c42e317127925c
Liu Xiao <liuxiaogenius@gmail.com>
2018-08-01 13:49:32 +0800

    various updates

--- maven-dependencies/flink-quickstart/generate.bat
+++ maven-dependencies/flink-quickstart/generate.bat
@@ -1,4 +1,4 @@
-SET FLINK_VER=1.3.1
+SET FLINK_VER=1.5.2
 
-CALL mvn archetype:generate -DarchetypeGroupId=org.apache.flink -DarchetypeArtifactId=flink-quickstart-java -DarchetypeVersion=%FLINK_VER% -DinteractiveMode=false -DgroupId=flink_quickstart -DartifactId=java
-CALL mvn archetype:generate -DarchetypeGroupId=org.apache.flink -DarchetypeArtifactId=flink-quickstart-scala -DarchetypeVersion=%FLINK_VER% -DinteractiveMode=false -DgroupId=flink_quickstart -DartifactId=scala
+CALL mvn archetype:generate -DarchetypeGroupId=org.apache.flink -DarchetypeArtifactId=flink-quickstart-java -DarchetypeVersion=%FLINK_VER% -DinteractiveMode=false -DgroupId=org.apache.flink -DartifactId=flink-quickstart-java
+CALL mvn archetype:generate -DarchetypeGroupId=org.apache.flink -DarchetypeArtifactId=flink-quickstart-scala -DarchetypeVersion=%FLINK_VER% -DinteractiveMode=false -DgroupId=org.apache.flink -DartifactId=flink-quickstart-scala
