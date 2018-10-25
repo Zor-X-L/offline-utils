38fed0162c01c2900398d84890c4b94011fc5fd3
Liu Xiao <liuxiaogenius@gmail.com>
2018-10-25 09:06:56 +0800

    flink-quickstart: update to 1.6.1

--- maven-dependencies/flink-quickstart/generate.bat
+++ maven-dependencies/flink-quickstart/generate.bat
@@ -1,4 +1,4 @@
-SET FLINK_VER=1.5.2
+SET FLINK_VER=1.6.1
 
 CALL mvn archetype:generate -DarchetypeGroupId=org.apache.flink -DarchetypeArtifactId=flink-quickstart-java -DarchetypeVersion=%FLINK_VER% -DinteractiveMode=false -DgroupId=org.apache.flink -DartifactId=flink-quickstart-java
 CALL mvn archetype:generate -DarchetypeGroupId=org.apache.flink -DarchetypeArtifactId=flink-quickstart-scala -DarchetypeVersion=%FLINK_VER% -DinteractiveMode=false -DgroupId=org.apache.flink -DartifactId=flink-quickstart-scala
